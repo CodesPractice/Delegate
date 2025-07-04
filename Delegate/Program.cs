e1d2867347fff40b785bce6d60d99488ea44b7d0
Dinushika <dprasangani@gmail.com>
2025-07-04 19:50:35 +0530

    Created delegate instances for calculation methods

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -4,17 +4,30 @@ namespace Delegate
 {
     internal class Program
     {
-        public delegate void GreetingDelegateObj(string name);
+        public delegate void GreetingDelegate(string name);
 
-        public delegate int CalculateTwoNumbers(int val1, int val2);
+        public delegate int CalculateTwoNumbersDelegate(int val1, int val2);
 
         static void Main(string[] args)
         {
             Greeting greetingObj = new Greeting();
-            GreetingDelegateObj greet = greetingObj.SayHi;
+            GreetingDelegate greet = greetingObj.SayHi;
             greet("Andrew ");
 
+            Console.WriteLine();
 
+
+           Calculation CalObj = new Calculation();
+            CalculateTwoNumbersDelegate AddNumsObj = CalObj.Addition;
+            CalculateTwoNumbersDelegate DeductNumsObj = CalObj.Deduction;
+            CalculateTwoNumbersDelegate MulNumsObj = CalObj.Multiplication;
+            CalculateTwoNumbersDelegate DivNumsObj = CalObj.Division;
+
+            Console.WriteLine("Additions : " + AddNumsObj(10, 20));
+            Console.WriteLine("Deduction : " + DeductNumsObj(10, 20));
+            Console.WriteLine("Muliplication : " + MulNumsObj(10, 20));
+            Console.WriteLine("Division : " + DivNumsObj(100, 20));
+            
 
             Console.ReadKey();
         }
