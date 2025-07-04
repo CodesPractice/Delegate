04d89c3eed28f242df9b45c6509b7472c0c3f3fe
Dinushika <dprasangani@gmail.com>
2025-07-04 19:36:18 +0530

    Add project files.

---
+++ Delegate/BasicDelegates/Calculation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.BasicDelegates
+{
+    internal class Calculation
+    {
+        public int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Deduction(int x, int y)
+        {
+            if (x > y)
+                return x - y;
+            else
+                return y - x;
+        }
+
+        public int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Division(int x, int y)
+        {
+            if (y != 0)
+                return x / y;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using Delegate.BasicDelegates;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        public delegate void GreetingDelegateObj(string name);
+
+        public delegate int CalculateTwoNumbers(int val1, int val2);
+
+        static void Main(string[] args)
+        {
+            Greeting greetingObj = new Greeting();
+            GreetingDelegateObj greet = greetingObj.SayHi;
+            greet("Andrew ");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
